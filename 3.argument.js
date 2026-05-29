aef015df3e15c85993b00a08c410bb5405d814b2
asm-saim <saim777m@gmail.com>
2026-05-29 07:24:03 +0600

    argumrnt completed

---
+++ 3.argument.js
@@ -0,0 +1,12 @@
+// argument may a variable. Not necessary that argument will always a number.
+//parameter always works inside function.
+
+
+function subtraction(input1, input2){
+    let sub=input1-input2;
+    console.log(input1, input2, 'subtraction value is =',sub);
+}
+let number1=12234;
+let number2=5636;
+subtraction(number1,number2);
+
